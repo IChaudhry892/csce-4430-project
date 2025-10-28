8fc4cf2680abdace14f4f7ce98459311d8fa7259
ChaseRichardson <ChaseARichardson2003@gmail.com>
2025-10-28 13:31:20 -0500

    added caclulate_ahead_vehicle_gap function to vehicle.py fixed inccorect value in simconfig

--- pygame-traffic-test/SimulationToolbox/SimulationConfig.py
+++ pygame-traffic-test/SimulationToolbox/SimulationConfig.py
@@ -10,6 +10,6 @@ class SimulationConfig:
     STOP_REAL_TIME = 30.0   # Stop simulation after 30 real-time seconds
 
     ROAD_IDS = {"Vertical Road": "vertical_road", "Horizontal Road": "horizontal_road"}
-    TRAFFIC_INTENSITIES = {"high": 15, "medium": 0.10, "low": 0.05} # Chance of spawning a vehicle per virtual minute per road
+    TRAFFIC_INTENSITIES = {"high": .15, "medium": 0.10, "low": 0.05} # Chance of spawning a vehicle per virtual minute per road
     TRAFFIC_SIGNAL_STATES = {"Red": "signal_red", "Green": "signal_green"}
     VEHICLE_STATES = {"moving": "moving", "waiting": "waiting"}
--- pygame-traffic-test/Vehicle/Vehicle.py
+++ pygame-traffic-test/Vehicle/Vehicle.py
@@ -2,7 +2,7 @@ from Animation.Animatable import Animatable
 from SimulationToolbox.Simulatable import Simulatable
 from Graphics.SimulationGraphicConfig import SimulationGraphicConfig
 from SimulationToolbox.SimulationConfig import SimulationConfig
-
+from Road.Road import Road
 class Vehicle(Animatable, Simulatable):
     def __init__(self, width, height, velocity, image, road_id, lane_id):
         self.width = width
@@ -29,6 +29,40 @@ class Vehicle(Animatable, Simulatable):
     def is_off_screen(self):
         return self.x < 0 - self.width or self.y < 0 - self.width
     
+    # Finds the gap between a vehicle and the ahead vehicle
+    def calculate_ahead_vehicle_gap(self):
+        road = None 
+        for component in self.scenario.getComponents(): # 
+            if isinstance(component, Road) and component.road_id == self.road_id:
+                road = component
+        lanelist = road.vehicle_lanes[self.road_id] # Current lane of the current road of the current vehicle
+
+        vehicles = []
+        for vehicle in lanelist: # Find all vehicles that are not current vehicle
+            if vehicle is not self:
+                vehicles.append(vehicle)
+
+        aheadvehicles = []
+        if self.road_id == "horizontal_road":
+            for vehicle in vehicles:
+                if vehicle.x < self.x:
+                    aheadvehicles.append(vehicle)
+            nearestvehicle = max(aheadvehicles, key=lambda v: v.x)
+            return (self.y - nearestvehicle.y) - nearestvehicle.height # returns the pixel gape between a vehicle and the nearest ahead vehicle
+        elif self.road_id == "vertical_road":
+            for vehicle in vehicles:
+                if vehicle.y < self.y:
+                    aheadvehicles.append(vehicle)
+            nearestvehicle = max(aheadvehicles, key=lambda v: v.y)
+            return (self.y - nearestvehicle.y) - nearestvehicle.height # returns the pixel gape between a vehicle and the nearest ahead vehicle
+
+
+
+
+
+
+        return 
+
     def simulate(self, delta_time):
         self.delta_time = delta_time
         signal = self.scenario.get_signal_for_road(self.road_id)
